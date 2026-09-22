c2303e126695509f73629315b5c36a52353b2d51
fcapparuccia-dot <f.capparuccia@gmail.com>
2026-09-22 13:15:10 +0000

    Aggiungi favicon personalizzata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,10 @@
 </script>
 
 <svelte:head>
+	<title>Meteo Mediator</title>
 	<link rel="icon" href={favicon} />
+	<link rel="apple-touch-icon" href={favicon} />
+	<meta name="theme-color" content="#1683c4" />
 </svelte:head>
 
 {@render children()}
